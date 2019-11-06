ba6f5083619d96f12e551225faee65d8b71550dc
Laksono Bramantio <laxbramantio@gmail.com>
2019-11-06 13:42:23 +0700

    Initial commit

---
+++ src/test/java/apap/tugas/situ/SituApplicationTests.java
@@ -0,0 +1,16 @@
+package apap.tugas.situ;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SituApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
