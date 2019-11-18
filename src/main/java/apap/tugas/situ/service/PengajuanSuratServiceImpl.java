cd64f134dfda22cff0a3f4fff53c29cefbb9e2a6
Nadia <nadia.syafitri@ui.ac.id>
2019-11-18 19:34:27 +0700

    feat: init

--- src/main/java/apap/tugas/situ/service/PengajuanSuratServiceImpl.java
+++ src/main/java/apap/tugas/situ/service/PengajuanSuratServiceImpl.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 @Service
 @Transactional
@@ -21,4 +24,41 @@ public class PengajuanSuratServiceImpl implements PengajuanSuratService {
         return pengajuanSuratDb.findAll();
     }
 
+    @Override
+    public Optional<PengajuanSuratModel> getSuratbyId(Integer id) {
+        return pengajuanSuratDb.findById(id);
+    }
+
+    @Override
+    public void generateCode(PengajuanSuratModel surat) {
+        String myCode = "";
+        String alphabet = "0123456789";
+        StringBuilder sb = new StringBuilder(8);
+        for (int j=0;j<8;j++) {
+            int index = (int) (alphabet.length() * Math.random());
+            sb.append(alphabet.charAt(index));
+        }
+        myCode = sb.toString();
+        SimpleDateFormat format = new SimpleDateFormat("ddMMyy");
+        String tahunTerbit = format.format(surat.getTanggalDisetujui());
+        myCode += String.valueOf(tahunTerbit);
+        surat.setNomorSurat(myCode);
+        pengajuanSuratDb.save(surat);
+    }
+
+    @Override
+    public PengajuanSuratModel ubahSurat(PengajuanSuratModel surat) {
+        PengajuanSuratModel mySurat = pengajuanSuratDb.findById(surat.getId()).get();
+
+        try{
+            mySurat.setTanggalDisetujui(surat.getTanggalDisetujui());
+            mySurat.setStatus(surat.getStatus());
+            generateCode(surat);
+            pengajuanSuratDb.save(mySurat);
+            return mySurat;
+        }catch (Exception e){
+            return null;
+        }
+    }
 }
+
