b680c876b8b217a8936699bf4ce24fb2692a5e17
Abiyu Muhammad Akmal <abiyouakmal@gmail.com>
2019-11-20 16:31:50 +0700

    feat: add feature 4 and 5 in add and delete Jenis Surat Implementation

---
+++ src/main/java/apap/tugas/situ/service/JenisSuratService.java
@@ -0,0 +1,16 @@
+package apap.tugas.situ.service;
+
+import apap.tugas.situ.model.JenisSuratModel;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface JenisSuratService {
+    List<JenisSuratModel> getSemuaJenisSurat();
+    Optional<JenisSuratModel> getJenisSuratById(Integer id);
+    Optional<JenisSuratModel> getJenisSuratByNama(String nama);
+    void tambahJenisSurat(JenisSuratModel jenisSuratBaru);
+    void hapusJenisSurat(Integer id);
+
+    boolean checkAddSuccess(String namaJenisSuratAdded, String namaJenisSuratDiDB);
+}
